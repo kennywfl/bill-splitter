35b9927320dceee897262e031cbb5f0ff08e0df6
LohZhenWei <iceraiser1@gmail.com>
2018-06-07 15:28:18 +0800

    Further enhance Nearby API

--- app/src/main/java/com/example/fa/billspliter/ui/adapter/EndPointConnectionCallbackAdapter.kt
+++ app/src/main/java/com/example/fa/billspliter/ui/adapter/EndPointConnectionCallbackAdapter.kt
@@ -33,7 +33,6 @@ class EndPointConnectionCallbackAdapter:EndpointDiscoveryCallback {
     constructor(context:Context,data:String) : super() {
         DeviceList = ArrayList<DeviceData>()
         DeviceListDialogFactory = DialogFactory()
-        Log.d("founded", " test123");
         DeviceListDialogFactory!!.showNearbyDialog(context,DeviceList!!,data)
     }
 }
